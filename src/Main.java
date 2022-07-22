09c92f219e990309a899bd9834daf022446a8edf
Ilya Agafonov <107766160+Vurhis1@users.noreply.github.com>
2022-07-22 06:27:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int startCheck = 100; //у клиента на счёте 100 рублей
-        int addOnStartCheck1 = 1100; //Клиент пополнил счёт
+        int addOnStartCheck1 = 1000; //Клиент пополнил счёт 1000 руб
         boolean task = true; //условие, при которым бонус будет получен, либо нет
 
         int percent;
@@ -13,14 +13,17 @@ public class Main {
         }
         int bonus = addOnStartCheck1 / 100 * percent;
 
-        if (addOnStartCheck1 < 1100) {
+        if (addOnStartCheck1 < 1000) {
 
             System.out.println("Бонус не получен");
+            System.out.println("На вашем счету: " + (startCheck + addOnStartCheck1));
         } else {
             System.out.println("Итоговый бонус : " + bonus);
+            System.out.println("На вашем счёту: " + (startCheck + addOnStartCheck1 + bonus));
         }
     }
 }
+
 
 
 
